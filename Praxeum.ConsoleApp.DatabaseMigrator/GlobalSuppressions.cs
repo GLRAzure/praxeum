44f245f6c0e8fba8e618a49fce0cb29daaa73b80
Matt Ruma <mattruma@gmail.com>
2019-02-20 10:49:39 -0500

    troubleshooting functions

--- Praxeum.ConsoleApp.DatabaseMigrator/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:Praxeum.ConsoleApp.DatabaseMigrator.Program.Main(System.String[])")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:Praxeum.ConsoleApp.DatabaseMigrator.Program.MainAsync~System.Threading.Tasks.Task")]
-
