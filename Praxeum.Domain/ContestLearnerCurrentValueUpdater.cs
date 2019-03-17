0b0d9a18fbd23782ee9232a079205bde39420011
Bryan Dykstra <dykstra.bryan@outlook.com>
2019-03-17 15:50:57 -0400

    Update ContestLearnerCurrentValueUpdater.cs

--- Praxeum.Domain/ContestLearnerCurrentValueUpdater.cs
+++ Praxeum.Domain/ContestLearnerCurrentValueUpdater.cs
@@ -24,17 +24,17 @@ namespace Praxeum.Domain
                 case ContestType.AccumulatedLevels:
                     contestLearner.CurrentValue = 
                         microsoftProfile.GameStatus.Level.LevelNumber - contestLearner.StartValue;
-                    break;
-                case ContestType.Levels:
-                    contestLearner.CurrentValue =
-                        microsoftProfile.GameStatus.Level.LevelNumber;
-                    break;
+                    break;                
                 case ContestType.AccumulatedPoints:
                 case ContestType.Leaderboard:
                     contestLearner.CurrentValue =
                         _experiencePointsCalculator.Calculate(
                             microsoftProfile.GameStatus.Level.LevelNumber,
                             microsoftProfile.GameStatus.CurrentLevelPointsEarned) - contestLearner.StartValue;
+                    break;
+                case ContestType.Levels:
+                    contestLearner.CurrentValue =
+                        microsoftProfile.GameStatus.Level.LevelNumber;
                     break;
                 case ContestType.Points:
                     contestLearner.CurrentValue =
